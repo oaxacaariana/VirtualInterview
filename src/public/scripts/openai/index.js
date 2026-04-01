29ae618259009fe83e522e5020f189e2802c9312
henryhafner <hhafner@trinity.edu>
2026-03-31 23:13:38 -0500

    made eye tracking run smoother
    working on calibration now

--- src/public/scripts/openai/index.js
+++ src/public/scripts/openai/index.js
@@ -368,97 +368,121 @@ elements.setupBtn.addEventListener('click', () => {
 });
 
 const videoFeedEl = document.getElementById('video-feed');
+const smileyFace = document.getElementById('smiley-face');
 const cameraContainer = document.querySelector('.camera-container');
-const dot = document.getElementById('gaze-dot');
-const smoothing = 0.15;
+const webgazer = window.webgazer;
 
 let webgazerInitialized = false;
-let currentX = 0;
-let currentY = 0;
-let targetX = 0;
-let targetY = 0;
+let total = 0;
+let count = 0;
 
-// Turns the camera on and begins facial expression and eye detection
+// Turns the camera on and begins eye detection
 async function startCamera() {
   console.log('Starting camera');
+
   // Set the video's stream to the user's camera
   const stream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: false
   });
+
   videoFeedEl.srcObject = stream;
   cameraContainer.classList.add('active');
 
-  // Loading models for pre-trained machine learning facial detection agent
-  await Promise.all([
-    faceapi.nets.ssdMobilenetv1.loadFromUri('../../models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('../../models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('../../models'),
-    faceapi.nets.ageGenderNet.loadFromUri('../../models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('../../models')
-  ]);
-
-  // Gather face data from frame of video per interval
-  setInterval(async () => {
-    let faceAIData = await faceapi.detectAllFaces(videoFeedEl).withFaceLandmarks().withFaceDescriptors().withAgeAndGender().withFaceExpressions();
-    console.log(faceAIData);
-  }, 500);
-
-  // Initialize webgazer
+  // Initialize webgazer (first time turning on the camera)
   if (!webgazerInitialized) {
     console.log('Initializing webgazer');
+
+    webgazer.params.faceMeshBasePath = "/mediapipe/face_mesh/";
     webgazer.setGazeListener((data, elapsedTime) => {
-      if (data == null) {
-        return;
-      }
-      targetX = data.x;
-      targetY = data.y;
-      console.log(elapsedTime);
+      if (!data) { return; } // data.x = x-coordinates and data.y = y-coordinates
+
+      // If the eye tracker is on the interviewer
+      if (isOverlap(data.x, data.y)) { count++; }
+      total++;
     }).begin();
 
-    // Smoothly move dot around screen
-    function animate() {
-      currentX += (targetX - currentX) * smoothing;
-      currentY += (targetY - currentY) * smoothing;
+    webgazer.showVideo(false);
+    webgazer.showFaceOverlay(false);
+    webgazer.showFaceFeedbackBox(false);
 
-      dot.style.left = currentX + "px";
-      dot.style.top = currentY + "px";
-      requestAnimationFrame(animate);
-    }
-    animate();
     webgazerInitialized = true;
   }
   else {
-    // Resume webgazer
+    // Resume webgazer (not first time turning on the camera)
     webgazer.resume();
   }
 }
 
-// Turns the camera video tracks off and the expression and eye tracking
+// Turns the camera video tracks off and pauses eye tracking
 function stopCamera() {
   console.log('Stopping Camera');
+
   // If there are no video tracks
-  if (!videoFeedEl.srcObject) {
-    return;
-  }
+  if (!videoFeedEl.srcObject) { return; }
   else {
     // Remove the track from the stream and set the video's source to null
     videoFeedEl.srcObject.getTracks().forEach(track => track.stop());
     videoFeedEl.srcObject = null;
     cameraContainer.classList.remove('active');
 
-    // Pause the webgazer
+    // Pause the webgazer and calculate eye contact %
     webgazer.pause();
+    console.log('accuracy: ' + (count / total) * 100 + '%');
+    count = 0;
+    total = 0;
   }
 }
 
-elements.cameraBtn.addEventListener('click', async () => {
-  if (videoFeedEl.srcObject) {
-    stopCamera();
-  } else {
-    await startCamera();
-  }
+// Checks whether the eye tracker is on the interviewer
+function isOverlap(xPos, yPos) {
+  const rect = smileyFace.getBoundingClientRect();
+  return (
+    xPos >= rect.left &&
+    xPos <= rect.right &&
+    yPos >= rect.top &&
+    yPos <= rect.bottom
+  );
+}
+
+// Toggles the camera
+elements.cameraBtn.addEventListener('click', () => {
+  if (videoFeedEl.srcObject) { stopCamera(); }
+  else { startCamera(); }
 });
+
+
+
+
+
+
+
+const calibrateContainer = document.querySelector('.calibrate-container');
+const calibrateBtn = document.querySelectorAll('.calibrate-btn');
+let isOpen = false;
+
+// Toggles the calibration screen
+calibrateBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if (!isOpen) {
+      calibrateContainer.classList.add('active');
+      isOpen = true;
+    }
+    else {
+      calibrateContainer.classList.remove('active');
+      isOpen = false;
+    }
+  })
+})
+
+
+
+
+
+
+
+
+
 
 elements.endBtn.addEventListener('click', async () => {
   await completeInterview('Interview marked complete. Thanks for practicing!');
